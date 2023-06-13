b2accbf63d4465432bece7545a3b669cb0c1f863
Ashok-2017 <84613826+Ashok-2017@users.noreply.github.com>
2023-06-13 13:39:07 +0800

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,12 @@
+variable "region" {
+   default = "us-west-2"
+}
+variable "cidr" {
+  cidr_block = ["10.119.23.42/16", "10.112.23.34/24"]
+}
+
+variable "subnets" {
+   subnets = ["subnet_a", "subnet_b"]
+}
+
+
