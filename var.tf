72d524ac4f94be60f6689a58b316c4eafd237371
Ashok-2017 <84613826+Ashok-2017@users.noreply.github.com>
2023-06-13 14:16:36 +0800

    Update var.tf

--- var.tf
+++ var.tf
@@ -2,11 +2,11 @@ variable "region" {
    default = "us-west-2"
 }
 variable "cidr" {
-  cidr_block = ["10.119.23.42/16", "10.112.23.34/24"]
+  default = ["10.119.23.42/16", "10.112.23.34/24"]
 }
 
 variable "subnets" {
-   subnets = ["subnet_a", "subnet_b"]
+   default = ["subnet_a", "subnet_b"]
 }
 
 variable "domain_name" {
